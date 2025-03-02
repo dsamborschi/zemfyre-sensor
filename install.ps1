651d5a5f624e4fd669d93b1e344901b327ae799d
dsamborschi <dsamborschi@gmail.com>
2025-03-01 19:01:45 -0500

    cleanup

--- install.ps1
+++ install.ps1
@@ -1,31 +1,26 @@
-# Define GitHub repository URL and local folder
+
 $repoUrl = "https://github.com/dsamborschi/zemfyre-sensor.git"
 $localFolder = "C:\"
 
-# Check if the folder exists, create if it doesn't
+
 if (-not (Test-Path -Path $localFolder)) {
     New-Item -Path $localFolder -ItemType Directory
 }
 
-# Navigate to the local folder
+
 Set-Location -Path $localFolder
 
-# Clone the repository from GitHub
 git clone $repoUrl
 
-# Navigate to the cloned repository folder
-$repoName = "zemfyre-sensor"  # Replace with your repository name
+$repoName = "zemfyre-sensor" 
 Set-Location -Path "$localFolder\$repoName"
 
-# Check if Docker Compose is installed
 $dockerCompose = Get-Command docker-compose -ErrorAction SilentlyContinue
 if (-not $dockerCompose) {
     Write-Host "Docker Compose is not installed. Please install Docker Compose before proceeding."
     exit
 }
 
-# Run Docker Compose
 docker-compose up -d
 
-# Print success message
 Write-Host "Docker Compose is now running the services."
