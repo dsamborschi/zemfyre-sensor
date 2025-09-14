83f1bc67b3ba71dcea5ec50700080370cd622054
dsamborschi <dsamborschi@gmail.com>
2025-09-13 23:52:48 -0400

    fix: update NTP server configuration comment and refresh Grafana database

--- bin/ntp_win_setup_server.bat
+++ bin/ntp_win_setup_server.bat
@@ -1,6 +1,6 @@
 @echo off
 REM ==========================
-REM Configure and enable Windows NTP server, TEST3
+REM Configure and enable Windows NTP server
 REM ==========================
 
 echo Ensuring W32Time service is set to start automatically...
