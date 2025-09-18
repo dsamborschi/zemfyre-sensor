59b8bf5b53fb9be82d1c6cc3ae8732fb775ed158
dsamborschi <dsamborschi@gmail.com>
2025-09-18 16:44:21 -0400

    fix: add MODE argument to upgrade_docker_containers function

--- bin/install.sh
+++ bin/install.sh
@@ -238,6 +238,7 @@ function upgrade_docker_containers() {
     sudo -u ${USER} env \
         DOCKER_TAG="${DOCKER_TAG}" \
         GIT_BRANCH="${BRANCH}" \
+        MODE="build" \
         "${UPGRADE_SCRIPT_PATH}"
 }
 
