477e56abe79c3f7f80a0dcdf8c76533d4be02d15
dsamborschi <dsamborschi@gmail.com>
2025-09-21 17:30:46 -0400

    Remove build_appengine.sh and add install_appEngine.sh for improved installation process

--- bin/run_upgrade.sh
+++ bin/run_upgrade.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://scripts.iotistic.ca/install)
+
+# Install or upgrade iotistic (formerly docker) engine
+bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://scripts.iotistic.ca/install-appengine)
