a0276a29c6b44f34deba5c5e3ad2fb5126182fc4
dsamborschi <dsamborschi@gmail.com>
2025-10-02 20:31:52 -0400

    feat(livepush): Add live code synchronization module for rapid development

--- bin/run_upgrade.sh
+++ bin/run_upgrade.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+# latest version    
 bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://scripts.iotistic.ca/install)
+
+# specific version
+# tag="v1.2.3"
+bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://scripts.iotistic.ca/install) v1.2.3
 
 # Install or upgrade iotistic (formerly docker) engine
 bash <(curl -H 'Cache-Control: no-cache' -sL --proto '=https' https://scripts.iotistic.ca/install-engine)
