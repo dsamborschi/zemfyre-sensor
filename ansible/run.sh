6f10f4813712f3cf1247c51761d60b8b3e94213a
dsamborschi <dsamborschi@gmail.com>
2025-10-16 21:08:41 -0400

    feat: Add kiosk deployment playbook and update run script to use it

--- ansible/run.sh
+++ ansible/run.sh
@@ -26,4 +26,4 @@ docker run --rm -it \
   -e ANSIBLE_HOST_KEY_CHECKING=False \
   -v "$PROJECT_ROOT:/workspace" \
   $IMAGE_NAME \
-  ansible-playbook -i /workspace/ansible/hosts.ini /workspace/ansible/deploy.yml "$@"
+  ansible-playbook -i /workspace/ansible/hosts.ini /workspace/ansible/deploy.kiosk.yml "$@"
